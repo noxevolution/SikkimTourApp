311a0044525f821c06ecf8b2500297f133e190ae
noxevolution <proloyd4@gmail.com>
2019-07-06 11:25:34 +0530

    Created a very ugly Splash Screen!!

---
+++ app/src/main/java/com/example/sikkimtourapp/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.sikkimtourapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
